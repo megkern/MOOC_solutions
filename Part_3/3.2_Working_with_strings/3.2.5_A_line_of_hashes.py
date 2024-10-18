0fc475c36038e0b9e37237609d9f47cadb35c698
megkern <99364827+megkern@users.noreply.github.com>
2024-10-18 17:12:11 -0500

    Create 3.2.5_A_line_of_hashes.py

---
+++ Part_3/3.2_Working_with_strings/3.2.5_A_line_of_hashes.py
@@ -0,0 +1,13 @@
+# Please write a program which prints out a line of hash characters, 
+# the width of which is chosen by the user.
+#
+# Sample output
+# Width: 3
+# ##
+
+hash = ""
+w = int(input("Width: "))
+while len(hash) < w:
+    hash += "#"
+
+print(hash)
