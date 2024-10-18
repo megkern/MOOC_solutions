16dd0b3384753d54033efd27a4b111075e2808ac
megkern <99364827+megkern@users.noreply.github.com>
2024-10-18 18:18:53 -0500

    Create 3.1.2_Fix_the_code:_Countdown.py

---
+++ Part_3/3.1_Loops_with_conditions/3.1.2_Fix_the_code:_Countdown.py
@@ -0,0 +1,27 @@
+# Programming exercise: Fix the code: Countdown
+#
+# The program below has some syntactic issues:
+#
+# print("Are you ready?")
+# number = int(input("Please type in a number: "))
+# while number = 0:
+# print(number)
+# print("Now!")
+# Please fix it so that it prints out the following:
+#
+# Sample output
+# Are you ready?
+# Please type in a number: 5
+# 5
+# 4
+# 3
+# 2
+# 1
+# Now!
+
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+while number > 0:
+    print(number)
+    number -= 1
+print("Now!")
