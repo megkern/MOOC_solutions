af591cfeca5faf6e60f5270dbe0f0d642a7ea60b
megkern <99364827+megkern@users.noreply.github.com>
2024-10-18 18:16:07 -0500

    Create 3.1.3_Numbers.py

---
+++ Part_3/3.1_Loops_with_conditions/3.1.3_Numbers.py
@@ -0,0 +1,18 @@
+# Programming exercise: Numbers
+#
+# Please write a program which asks the user for a number. 
+# The program then prints out all integer numbers greater than zero but smaller than the input.
+#
+# Sample output:
+# Upper limit: 5
+# 1
+# 2
+# 3
+# 4
+
+ul = int(input("Upper limit: "))
+num = 1
+
+while ul > 0 and num < ul:
+    print(num)
+    num += 1
