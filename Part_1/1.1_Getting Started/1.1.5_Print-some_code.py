355ce00326779fea37ae5496790b9fb0c781815c
megkern <99364827+megkern@users.noreply.github.com>
2024-10-21 09:34:24 -0500

    Create 1.1.5_Print-some_code.py

---
+++ Part_1/1.1_Getting Started/1.1.5_Print-some_code.py
@@ -0,0 +1,8 @@
+# Programming exercise: Print some code
+# https://programming-24.mooc.fi/part-1/1-getting-started
+#
+# Please write a program which prints out the following:
+# Sample output:
+# print("Hello there!")
+
+print('print("Hello there!")')
