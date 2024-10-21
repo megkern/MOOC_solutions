a13f3684a4877e19a10f7ee8097a88aae59e1d1e
megkern <99364827+megkern@users.noreply.github.com>
2024-10-21 09:42:03 -0500

    Update 1.2.3_Name_and_address.py

--- Part_1/1.2_Information_from_the_user/1.2.3_Name_and_address.py
+++ Part_1/1.2_Information_from_the_user/1.2.3_Name_and_address.py
@@ -1,3 +1,20 @@
 # Programming exercise: Name and address
 # https://programming-24.mooc.fi/part-1/2-information-from-the-user
 #
+# Please write a program which asks for the user's name and address. 
+# The program should also print out the given information, as follows:
+# Given name: Steve
+# Family name: Sanders
+# Street address: 91 Station Road
+# City and postal code: London EC05 6AW
+# Steve Sanders
+# 91 Station Road
+# London EC05 6AW
+
+gname = input("Given name:")
+fname = input("Family name:")
+stadd = input("Street address:")
+cpadd = input("City and postal code:")
+print(gname + " " + fname)
+print(stadd)
+print(cpadd)
