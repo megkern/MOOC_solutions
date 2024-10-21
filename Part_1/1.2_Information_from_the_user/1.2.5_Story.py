160e16c2eae53888f63da86b55e5040a807bc2bc
megkern <99364827+megkern@users.noreply.github.com>
2024-10-21 09:44:19 -0500

    Update 1.2.5_Story.py

--- Part_1/1.2_Information_from_the_user/1.2.5_Story.py
+++ Part_1/1.2_Information_from_the_user/1.2.5_Story.py
@@ -1,3 +1,17 @@
 # Programming exercise: Story
 # https://programming-24.mooc.fi/part-1/2-information-from-the-user
 #
+# Please write a program which prints out the following story. 
+# The user gives a name and a year, which should be inserted into the printout.
+# Sample output:
+# Please type in a name: Mary
+# Please type in a year: 1572
+# 
+# Mary is a valiant knight, born in the year 1572. One morning Mary woke up to an awful racket: 
+# a dragon was approaching the village. Only Mary could save the village's residents.
+# 
+# The story should change according to the input given by the user.
+
+name = input("Please type in a name:")
+year = input("Please type in a year:")
+print(name + " is a valiant knight, born in the year " + year + ". One morning " + name + " woke up to an awful racket: a dragon was approaching the village. Only " + name + " could save the village's residents.")
