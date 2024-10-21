4647f7260b11da6b2cf0f162ac2ea4e269d821c4
megkern <99364827+megkern@users.noreply.github.com>
2024-10-21 09:33:08 -0500

    Create 1.1.4_Minutes_in_a_year.py

---
+++ Part_1/1.1_Getting Started/1.1.4_Minutes_in_a_year.py
@@ -0,0 +1,8 @@
+# Programming exercise: Minutes in a year
+# https://programming-24.mooc.fi/part-1/1-getting-started
+#
+# Please write a program which prints out the number of minutes in a year. 
+# Use Python code to perform the calculation, as in the previous code example.
+
+print("Minutes in a year:")
+print(365*24*60) # 365 days, 24 hours in each day, 60 minutes in each hour
