44e39456c45b092fbd5d95a81bb5e505797d59c8
megkern <99364827+megkern@users.noreply.github.com>
2024-10-21 09:40:29 -0500

    Update 1.2.1_Name_twice.py

--- Part_1/1.2_Information_from_the_user/1.2.1_Name_twice.py
+++ Part_1/1.2_Information_from_the_user/1.2.1_Name_twice.py
@@ -1,3 +1,12 @@
 # Programming exercise: Name twice
 # https://programming-24.mooc.fi/part-1/2-information-from-the-user
 #
+# Please write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+# An example of the how the program should function:
+# What is your name? Paul
+# Paul
+# Paul
+
+name = input("What is your name? ")
+print(name)
+print(name)
