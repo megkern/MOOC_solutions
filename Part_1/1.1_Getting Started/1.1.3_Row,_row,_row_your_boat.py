e01223afe1fe9fdf1e9fd18cd2fc21f7bcd4d3fb
megkern <99364827+megkern@users.noreply.github.com>
2024-10-21 09:31:06 -0500

    Create 1.1.3_Row,_row,_row_your_boat.py

---
+++ Part_1/1.1_Getting Started/1.1.3_Row,_row,_row_your_boat.py
@@ -0,0 +1,12 @@
+# Programming exercise: Row, Row, Row Your Boat
+# https://programming-24.mooc.fi/part-1/1-getting-started
+# 
+# Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
+# 
+# Sample output:
+# Row, row, row your boat,
+# Gently down the stream.
+# Merrily, merrily, merrily, merrily,
+# Life is but a dream.
+
+print("Row, row, row your boat,\nGently down the stream.\nMerrily, merrily, merrily, merrily,\nLife is but a dream.")
