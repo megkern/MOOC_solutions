8ee230f8d721574b6b681cf6c69e8f3658060ee7
megkern <99364827+megkern@users.noreply.github.com>
2024-10-22 08:47:22 -0500

    Create 1.3.3_Arithmetics.py

---
+++ Part_1/1.3_More_about_variables/1.3.3_Arithmetics.py
@@ -0,0 +1,22 @@
+# Programming exercise: Arithmetics
+# https://programming-24.mooc.fi/part-1/3-more-about-variables
+#
+# This program already contains two integer variables, x and y:
+# x = 27
+# y = 15
+# Please complete the program so that it also prints out the following:
+# 27 + 15 = 42
+# 27 - 15 = 12
+# 27 * 15 = 405
+# 27 / 15 = 1.8
+
+x = 27
+y = 15
+sum = x + y
+dif = x - y
+prod = x * y
+quot = x / y
+print(f"27 + 15 = {sum}")
+print(f"27 - 15 = {dif}")
+print(f"27 * 15 = {prod}")
+print(f"27 / 15 = {quot}")
