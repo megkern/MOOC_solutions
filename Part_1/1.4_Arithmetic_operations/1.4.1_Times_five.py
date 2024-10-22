29dd020c2c0fb8b10ed96f67c408c67ca30f3b37
megkern <99364827+megkern@users.noreply.github.com>
2024-10-22 08:51:24 -0500

    Create 1.4.1_Times_five.py

---
+++ Part_1/1.4_Arithmetic_operations/1.4.1_Times_five.py
@@ -0,0 +1,13 @@
+# Programming exercise: Times five
+# https://programming-24.mooc.fi/part-1/4-arithmetic-operations
+# 
+# Please write a program which asks the user for a number. 
+# The program then prints out the number multiplied by five.
+# 
+# The program should function as follows:
+# Please type in a number: 3
+# 3 times 5 is 15
+
+num = int(input("Please type in a number"))
+prod = 5 * num
+print(f"{num} times 5 is {prod}")
