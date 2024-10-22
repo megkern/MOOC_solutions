34fff5ee4311f659fc94fa2e12713ca044d31021
megkern <99364827+megkern@users.noreply.github.com>
2024-10-22 08:42:38 -0500

    Create 1.3.1_Quiz:_Background_Information.py

---
+++ Part_1/1.3_More_about_variables/1.3.1_Quiz:_Background_Information.py
@@ -0,0 +1,2 @@
+# The course includes a questionnaire to gather background information on the learners.
+# This background information is used for scientific research and for developing the course material and exercises.
