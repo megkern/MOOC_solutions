e7a33395268a72a79ce7dd2adf31930cd08a32eb
megkern <99364827+megkern@users.noreply.github.com>
2024-10-22 08:48:56 -0500

    Create 1.3.4_Fix_the_code:_Print_a_single_line.py

---
+++ Part_1/1.3_More_about_variables/1.3.4_Fix_the_code:_Print_a_single_line.py
@@ -0,0 +1,21 @@
+# Programming exercise: Fix the code: Print a single line
+# https://programming-24.mooc.fi/part-1/3-more-about-variables
+#
+# Please fix this program so that the entire calculation, complete with result, 
+# is printed out on a single line. Do not change the number of print commands used.
+#
+# print(5)
+# print(" + ")
+# print(8)
+# print(" - ")
+# print(4)
+# print(" = ")
+# print(5 + 8 - 4)
+
+print(5, end="")
+print(" + ", end="")
+print(8, end="")
+print(" - ", end="")
+print(4, end="")
+print(" = ", end="")
+print(5 + 8 - 4, end="")
